d1d250f06043cb1bff311ace247ee162cbe2b9ce
SWTxSerega-tech <happycraft2468@gmail.com>
2026-01-21 21:00:52 +0200

    homework_commit_2

--- homework_num_1.py
+++ homework_num_1.py
@@ -1,14 +1,20 @@
 from datetime import datetime
 
 def get_days_from_today(date):
-    conv_date = datetime.strptime(date, '%Y.%m.%d').date() #Задана дата переводиться до datetime об'єкту в форматі "РРРР-ММ-ДД"
+    date_replace = date.replace('.', '-', 2).replace('/', '-', 2).replace(':', '-', 2) #Робимо заміну символу при неправильному вводі
+    conv_date = datetime.strptime(date_replace, '%Y-%m-%d').date() #Задана дата переводиться до datetime об'єкту в форматі "РРРР-ММ-ДД"
     today_date = datetime.today().date() #Поточна дата в форматі "РРРР-ММ-ДД"
-    total_date = conv_date - today_date #Різниця вказаної та поточної дати
-    
-    return total_date.days #Повернення результату різниці
+    total_date = conv_date - today_date #Різниця вказаної та поточної дати    
+    return total_date.days #Повернення результату різниці    
 
-date_plus = '2026.02.1' #Задаємо дату, яка виводить результат з числом "+"
+date_plus = '2026-02-01' #Задаємо дату, яка виводить результат з додатним числом
 print(f'Різниця між поточною та заданою датами: {get_days_from_today(date_plus)} дня(і)')
 
-date_minus = '2026.01.01' #Задаємо дату, яка виводить результат з число "-"
+date_minus = '2026.01.01' #Задаємо дату, яка виводить результат з від'ємним числом на неправильному написанні через "."
 print(f'Різниця між поточною та заданою датами: {get_days_from_today(date_minus)} дня(і)')
+
+date_with_slash = '2026/02/28' #Задаємо дату, яка виводить результат при неправильному написанні через "/"
+print(f'Різниця між поточною та заданою датами: {get_days_from_today(date_with_slash)} дня(і)')
+
+date_with_double_doot = '2026:03:15' #Задаємо дату, яка виводить результат при неправильному написанні через ":"
+print(f'Різниця між поточною та заданою датами: {get_days_from_today(date_with_double_doot)} дня(і)')
--- homework_num_2.py
+++ homework_num_2.py
@@ -1,12 +1,20 @@
 import random
 
-def get_numbers_ticket(min, max, quantity):
+""""
+Функція get_numbers_ticket, визначає переможця(-ців) у розігращі
+з унікальним номером квитка та відсортовує номери(у разі великої
+кількості цих номерів)
+"""""
+
+def get_numbers_ticket(min, max, quantity): 
+    empty_lst = []
     if min < 1 or max > 1000: #Робимо перевірку на мінімально та максимально допустимі значення
-        return 'Неправильно введене значення min або max'
+        return empty_lst 
     
     tickets = random.sample(range(min, max), quantity) #Виводимо вказану кількість унікальних номерів квитків вказану кількість разів
+    tickets.sort() #Сортуємо список
     return tickets
         
         
-print(get_numbers_ticket(1, 50, 6))
+print(f'Переможці у розіграші: {get_numbers_ticket(1000, 1200, 6)}')
         
