6dcad2740ca0051273c0c5704ec3bf14d6489d58
SWTxSerega-tech <happycraft2468@gmail.com>
2026-01-22 19:05:21 +0200

    homework_commit_5

--- homework_num_2.py
+++ homework_num_2.py
@@ -8,13 +8,18 @@ import random
 
 def get_numbers_ticket(min, max, quantity): 
     empty_lst = []
-    if min < 1 or max > 1000: #Робимо перевірку на мінімально та максимально допустимі значення
-        return empty_lst 
+    if min < 1 or max > 1000 or min > max: #Робимо перевірку на мінімально та максимально допустимі значення
+        return empty_lst
+    try:
+        tickets = random.sample(range(min, max), quantity) #Виводимо вказану кількість унікальних номерів квитків вказану кількість разів
+        tickets.sort() #Сортуємо список
+        return tickets
+    except:
+        return empty_lst #Робимо виняток у разі некоректно введених даних
     
-    tickets = random.sample(range(min, max), quantity) #Виводимо вказану кількість унікальних номерів квитків вказану кількість разів
-    tickets.sort() #Сортуємо список
-    return tickets
-        
 print(f'Переможці у розіграші: {get_numbers_ticket(1, 50, 6)}') #При введенні коректних даних       
 print(f'Переможці у розіграші: {get_numbers_ticket(1000, 1200, 6)}') #При введенні некоректних даних
+print(f'Переможці у розіграші: {get_numbers_ticket(1200, 1000, 6)}') #При введенні некоректних даних
+print(f'Переможці у розіграші: {get_numbers_ticket(10, 14, 6)}') #При введенні некоректних даних
+print(f'Переможці у розіграші: {get_numbers_ticket(14, 10, 6)}') #При введенні некоректних даних
         
